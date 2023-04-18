b83428f0736a26e962ee5b46290c0e44c20a05e5
Loai Khalid <78956649+loaimaklad@users.noreply.github.com>
2023-04-18 22:04:18 +0200

    Update Un-Hashing-by-knowing-code.py

--- Un-Hashing-by-knowing-code.py
+++ Un-Hashing-by-knowing-code.py
@@ -1,14 +1,10 @@
 #gui-built :
-from tkinter import *
-root=Tk()
-root.title('simp-unencrypt')
-root.geometry('300x300')
-x=Entry()
+x=Input('Whatis UR CODE : ')
 def Lo13I():
     try :
-        as1 = int(2009*80)
+        as1 = int(2009*80) #CODE THATIS IS USEING HASH CODE
         y=x-as1
         print(chr(y))
     except Exception as ex :
         print('An err was detected /\\')
-root.mainloop()
+
