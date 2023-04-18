fa530d5b955320e8298d13e8a5d2d14f64af06a9
Loai Khalid <78956649+loaimaklad@users.noreply.github.com>
2023-04-18 22:11:11 +0200

    Update Un-Hashing-by-knowing-code.py

--- Un-Hashing-by-knowing-code.py
+++ Un-Hashing-by-knowing-code.py
@@ -1,11 +1,10 @@
 #app is made by loai khalid by tkinter/python !-- Loay Copyright 2022 - 2023
 #WAIT FOR GUI BUILT
-x=input('Whatis UR CODE : ')
-def Lo13I():
+while True :
+    x=int(input('Whatis UR CODE : '))
     try :
         as1 = int(2009*80) #CODE THATIS IS USEING HASH CODE
         y=x-as1
         print(chr(y))
     except Exception as ex :
-        print('An err was detected /\\')
-
+        print(ex)
